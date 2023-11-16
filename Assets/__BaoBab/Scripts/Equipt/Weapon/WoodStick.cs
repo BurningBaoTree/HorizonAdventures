5adda4b228c26d5ba7c6fdbd7c5924d00cbec586
SengbumKim <bummykimmy@gmail.com>
2023-11-16 15:21:19 +0900

    WoodStick 모션 작업
    _

--- Assets/__BaoBab/Scripts/Equipt/Weapon/WoodStick.cs
+++ Assets/__BaoBab/Scripts/Equipt/Weapon/WoodStick.cs
@@ -4,4 +4,47 @@ using UnityEngine;
 
 public class WoodStick : EquiptBase
 {
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+       
+    }
+    protected override void UseActivate()
+    {
+        base.UseActivate();
+
+
+        if (!spRender.flipX)
+        {
+            anim.SetTrigger("Swing");
+        }
+        else if(spRender.flipX)
+        {
+            anim.SetTrigger("Swing_Left");
+        }
+
+    }
+
+    protected override void UseUtillActivate()
+    {
+        base.UseUtillActivate();
+
+
+    }
+
+    protected override void EquiptThisGear()
+    {
+        base.EquiptThisGear();
+
+        if(spRender.flipX)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, -27.47f);
+        }
+    }
 }
